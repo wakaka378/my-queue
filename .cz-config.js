adcf77f6915555656351d68fb6ddcc2e16dc85db
wakaka378 <gebilaofl@163.com>
2023-01-06 20:37:36 +0800

    :wrench: CI(custom): ceshi
    ceshi

--- .cz-config.js
+++ .cz-config.js
@@ -3,32 +3,32 @@
 module.exports = {
   types: [
     {
-      value: 'release',
-      name: '🚀  release:  发布版本/发布标签',
+      value: ':rocket: release',
+      name: '🚀 release: 发布版本/发布标签',
     },
     {
-      value: 'WIP',
-      name: '💪  WIP:      正在进行的工作',
+      value: ':construction: WIP',
+      name: '💪 WIP: 正在进行的工作',
     },
     {
-      value: 'feat',
-      name: '✨  feat:     新的特性',
+      value: ':sparkles: feat',
+      name: '✨ feat: 新的特性',
     },
     {
-      value: 'fix',
-      name: '🐛  fix:      修复Bug',
+      value: ':bug: fix',
+      name: '🐛 fix: 修复 Bug',
     },
     {
-      value: 'CI',
-      name: '🔧  CI:       目录结构变更(CI, Buiding, Tool...)',
+      value: ':wrench: CI',
+      name: '🔧 CI: 目录结构变更(CI, Buiding, Tool...)',
     },
 
     {
-      value: 'docs',
+      value: ':pencil: docs',
       name: '📝  docs:     仅文档更改',
     },
     {
-      value: 'merge',
+      value: ':twisted_rightwards_arrows: merge',
       name: '🔀  merge:    合并分支',
     },
     {
@@ -36,36 +36,36 @@ module.exports = {
       name: '⏪  revert:   版本回滚',
     },
     {
-      value: 'test',
+      value: ':white_check_mark: test',
       name: '✅  test:     添加缺失的测试或更正现有测试',
     },
     {
-      value: 'perf',
+      value: ':chart_with_upwards_trend: perf',
       name: '📈  perf:     提高性能的代码更改',
     },
     {
-      value: 'chore',
+      value: ':thought_balloon: chore',
       name: '🗯   chore:    不修改src或测试文件的更改。例如更新构建任务、包管理器',
     },
     {
-      value: 'ui',
+      value: ':lipstick: ui',
       name: '💄  UI:       更新UI和样式文件。',
     },
     {
-      value: 'style',
+      value: ':art: style',
       name: '🎨  style:    不影响代码含义的更改（空白、格式、缺少分号等）',
     },
     {
-      value: 'dep_up',
+      value: ':package: dep_up',
       name: '📦  dep_up:   更新已编译的文件或包。',
     },
 
     {
-      value: 'refactor',
+      value: ':hammer: refactor',
       name: '🔨  refactor: 既不修复错误也不添加功能的代码更改',
     },
     {
-      value: 'mv',
+      value: ':truck: mv',
       name: '🚚  mv:       移动或重命名文件。',
     },
   ],
