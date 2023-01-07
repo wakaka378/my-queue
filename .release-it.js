292233c33ef323d1018a60a185f5b94adaf46044
wakaka378 <gebilaofl@163.com>
2023-01-07 15:31:44 +0800

    :wrench: CI(custom): writerOpts
    release-it writerOpts changelog

---
+++ .release-it.js
@@ -0,0 +1,27 @@
+const fs = require('fs')
+
+const commitTemplate = fs.readFileSync('.cz-config.js').toString()
+
+module.exports = {
+  github: {
+    release: true,
+  },
+  git: {
+    commitMessage: 'release: release v${version}',
+  },
+  npm: {
+    publish: true,
+  },
+  hooks: {
+    'after:bump': 'echo 更新版本成功',
+  },
+  plugins: {
+    '@release-it/conventional-changelog': {
+      infile: 'CHANGELOG.md',
+      header: '# Changelog',
+      writerOpts: {
+        commitPartial: commitTemplate,
+      },
+    },
+  },
+}
